ffc050617c97073d4055fdc9e1d2cd690d0d358e
vuduong97 <ddtvu@whitespace.vn>
2024-01-23 23:02:35 +0700

    update procfile

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const compression = require("compression");
 const express = require("express");
+const { default: helmet } = require("helmet");
 const morgan = require("morgan");
 const wkhtmltopdf = require("wkhtmltopdf");
 
@@ -8,6 +9,7 @@ const app = express();
 
 // init middlewares
 app.use(morgan("dev"));
+app.use(helmet());
 app.use(compression());
 
 app.use(express.json());
